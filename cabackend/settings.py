c12dd173001ace39a346d56931c98d851bf4e05c
Anc0 <ak6688@student.uni-lj.si>
2019-03-27 14:24:03 +0100

    Debug enabled, nginx config somewhat working.

--- cabackend/settings.py
+++ cabackend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pbjdukkxg4y0egf^)ux$+ha$m2nbthot5hii=ued806+i@6k%e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 from .local_settings import DOTENV_PATH
