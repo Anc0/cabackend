ccb6f2f78ebab770642ed0a6da72ba036c2a200e
Anc0 <ak6688@student.uni-lj.si>
2019-03-29 09:21:53 +0100

    Added console logging to production configuration.

--- conf/conf.production.local_settings.py
+++ conf/conf.production.local_settings.py
@@ -3,3 +3,26 @@ import os
 MQTT_HOST_IP = "localhost"
 
 DOTENV_PATH = os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), '.env'), '.env')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{levelname}] {asctime} {module}: {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+    },
+}
