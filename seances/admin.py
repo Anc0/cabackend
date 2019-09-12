ea1725ab04ecdd04a106a5690a619952b4ef9930
Anc0 <ak6688@student.uni-lj.si>
2019-09-12 21:58:42 +0200

    Updated seance admin panel.

--- seances/admin.py
+++ seances/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from seances.models import Seance
 
-admin.site.register(Seance)
+
+class SeanceAdmin(admin.ModelAdmin):
+    list_display = ['start', 'end', 'user', 'valid']
+
+
+admin.site.register(Seance, SeanceAdmin)
