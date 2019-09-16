8731c03c9ae5cbc06efce7dee9a2a06bab616896
Anc0 <ak6688@student.uni-lj.si>
2019-09-16 19:16:30 +0200

    Added seance filter.

--- seances/admin.py
+++ seances/admin.py
@@ -3,8 +3,26 @@ from django.contrib import admin
 from seances.models import Seance
 
 
+class SeanceValidFilter(admin.SimpleListFilter):
+    title = "Is valid"
+    parameter_name = "Valid"
+
+    def lookups(self, request, model_admin):
+        return [
+            ('valid', 'Valid'),
+            ('not_valid', 'Not valid')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'valid':
+            return queryset.distinct().filter(valid=True)
+        elif self.value() == 'not_valid':
+            return queryset.distinct().filter(valid=False)
+
+
 class SeanceAdmin(admin.ModelAdmin):
     list_display = ['start', 'end', 'user', 'valid']
+    list_filter = (SeanceValidFilter, )
 
 
 admin.site.register(Seance, SeanceAdmin)
